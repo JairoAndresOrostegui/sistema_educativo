d3b56251d9d84cca7f69c943fd2ac14be1b4b25a
Llinas Web Deployment <deploy@liceobilinguerodolfollinas.edu.co>
2026-09-08 14:46:51 +0000

    Deploy production web d19f45a0b75a49555b00fb76c8200c89f9fa711a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c096","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.c48d525f67bd4f5286a6.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c096","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.fef0981de2dc8de13358.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "674840722" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2195314231" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
